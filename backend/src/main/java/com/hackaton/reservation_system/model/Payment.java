c69698fd591b73545629bdd7d0dc1a217c051325
Maksym Norokha <maxnorohich@gmail.com>
2024-11-16 16:24:42 +0100

    implemented payment entity

---
+++ backend/src/main/java/com/hackaton/reservation_system/model/Payment.java
@@ -0,0 +1,32 @@
+package com.hackaton.reservation_system.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String paymentDate;
+
+    @Column
+    private String paymentTime;
+
+    @Column
+    private String paymentAmount;
+
+    @Column
+    private String paymentMethod;
+
+    @Column
+    private String cardNumber;
+
+    @Column
+    private String cardHolder;
+
+}
